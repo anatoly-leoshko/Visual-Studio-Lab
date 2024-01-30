dbb66bab2988207e6196302388201f774333af14
Anatoly Leoshko <ana7oly.leoshko@gmail.com>
2024-01-30 20:38:57 +0200

    Add project files.

---
+++ Problem-3/Program.cs
@@ -0,0 +1,10 @@
+﻿string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+string email = Console.ReadLine();
+
+Console.Write("Customer: ");
+Console.Write(firstName);
+Console.Write(" " + lastName);
+Console.Write(" (" + email + ")");
+
+//Console.WriteLine($"Customer: {firstName}  {lastName} ({email})");Директно се пише на един ред.
